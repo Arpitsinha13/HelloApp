71939479458e7c3e545668adccad55be862862ed
Arpitsinha13 <arpitbinaysinha07@gmail.com>
2026-03-11 13:56:41 +0530

    Main branch added

---
+++ HelloApp.java
@@ -0,0 +1,12 @@
+class HelloApp{
+	public static void main(string[] args){
+		//system.out.println("Hello All ...");
+
+		int num = 100;
+
+		int marks[] = {10,20,30};
+
+		system.out.println(Arrays.tostring(marks));
+		system.out.println(num);
+	}
+}
